338efb1060d8b296bed35ecb08efb83ce25e36b7
anyms <mytellee@gmail.com>
2020-05-04 17:46:40 +0530

    updating README.md

--- app/src/main/java/app/spidy/browser/MainActivity.kt
+++ app/src/main/java/app/spidy/browser/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         browserFragment = BrowserFragment()
-        browserFragment.browserListener = BrowserListener()
+//        browserFragment.browserListener = BrowserListener()
         supportFragmentManager.beginTransaction()
             .add(R.id.browser_holder, browserFragment)
             .commit()
